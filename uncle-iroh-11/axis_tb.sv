197e52b126441f4cb21431bdfa15dacae9fdb910
Iztok Jeras <iztok.jeras@gmail.com>
2025-02-25 00:19:27 +0100

    failed attempt to avoid race condition without unit delays

--- uncle-iroh-11/axis_tb.sv
+++ uncle-iroh-11/axis_tb.sv
@@ -1,137 +1,161 @@
 `timescale 1ns/1ps
 
 module axis_tb;
- 
-  localparam  WORD_W=8, BUS_W=8, 
+
+  localparam  WORD_W=8, BUS_W=8,
               N_BEATS=10, WORDS_PER_BEAT=BUS_W/WORD_W,
-              PROB_VALID=10, PROB_READY=10,
+              PROB_VALID=30, PROB_READY=30,
               CLK_PERIOD=10, NUM_EXP=500;
 
-  logic clk=0, rstn=1;
-  logic s_ready, s_valid, m_ready, m_valid;
-  logic              [WORDS_PER_BEAT-1:0][WORD_W-1:0] s_data, m_data, in_beat;
-  logic [N_BEATS-1:0][WORDS_PER_BEAT-1:0][WORD_W-1:0] in_data, out_data, exp_data;
+  logic clk;
+  logic rstn;
+  // AXI-Stream
+  logic                                               valid;
+  logic                                               ready;
+  logic              [WORDS_PER_BEAT-1:0][WORD_W-1:0] data;
+  // local signals
+  logic              [WORDS_PER_BEAT-1:0][WORD_W-1:0] in_beat;
+  logic [N_BEATS-1:0][WORDS_PER_BEAT-1:0][WORD_W-1:0] tx_data, rx_data;
 
-  logic [N_BEATS*WORD_W*WORDS_PER_BEAT-1:0] queue [$];
+  // clock generator
+  // NOTE: not sure which assignment operator is correct, but I usually use =
+  initial                clk = 1'b1;
+  always #(CLK_PERIOD/2) clk = ~clk;
 
-  initial forever #(CLK_PERIOD/2) clk <= ~clk;
-
-  AXIS_Source #(.WORD_W(WORD_W), .BUS_W(BUS_W), .PROB_VALID(PROB_VALID), .N_BEATS(N_BEATS)) source (.*);
-  AXIS_Sink   #(.WORD_W(WORD_W), .BUS_W(BUS_W), .PROB_READY(PROB_READY), .N_BEATS(N_BEATS)) sink   (.*);
-
-  assign s_ready = m_ready;
-  assign m_data = s_data;
-  assign m_valid = s_valid;
+  // VIP instances
+  AXIS_TX #(.WORD_W(WORD_W), .BUS_W(BUS_W), .PROB_VALID(PROB_VALID), .N_BEATS(N_BEATS)) tx (.*);
+  AXIS_RX #(.WORD_W(WORD_W), .BUS_W(BUS_W), .PROB_READY(PROB_READY), .N_BEATS(N_BEATS)) rx (.*);
 
   initial begin
     $dumpfile ("dump.vcd"); $dumpvars;
     rstn = 0;
     repeat(5) @(posedge clk);
-    rstn = 1;
+    rstn <= 1;
     repeat(5) @(posedge clk);
 
-    repeat(NUM_EXP) begin
-      foreach (in_data[n]) begin
-        foreach (in_beat[w])
-          in_beat[w] = $urandom_range(0,2**WORD_W-1);
-        in_data[n] = in_beat;
-      end
-      queue.push_front(in_data); 
-// append to end of queue
-      #1
-      source.axis_push_packet;
+    // initialize reference data beat
+    foreach (tx_data[n]) begin
+      foreach (in_beat[w])
+        in_beat[w] = $urandom_range(0,2**WORD_W-1);
+      tx_data[n] = in_beat;
+    end
+    
+    // TX
+    for (int i=0; i<NUM_EXP; i++) begin: tx_loop
+      // drive data stream
+      tx.axis_push_packet(tx_data);
+    end: tx_loop
+    // wait for RX to finish
+    forever begin
+      @(posedge clk);
     end
   end
 
   initial begin
-    repeat(NUM_EXP) begin
-      sink.axis_pull_packet;
-      exp_data = queue.pop_back();
-      assert (exp_data == out_data) 
-// remove last element
-        $display("Outputs match: %d", exp_data);
-      else $fatal(0, "Expected: %h != Output: %h", exp_data, out_data);
-    end
+    // RX
+    for (int i=0; i<NUM_EXP; i++) begin: rx_loop
+      // clear RX data array
+      rx_data = 'x;
+      // sample data stream
+      rx.axis_pull_packet(rx_data);
+      // check data for correctness
+      assert (rx_data == tx_data)
+        $display("Packet[%0d]: Outputs match: %h", i, rx_data);
+      else $error("Packet[%0d]: Expected: %h != Received: %h", i, tx_data, rx_data);
+    end: rx_loop
+    repeat(5) @(posedge clk);
     $finish();
   end
-endmodule
+
+//  initial begin
+//    repeat(100) @(posedge clk);
+//    $display("TIMEOUT");
+//    $finish();
+//  end
+endmodule: axis_tb
 
 
+module AXIS_TX #(
+  parameter  WORD_W=8, BUS_W=8, PROB_VALID=20,
+             N_BEATS=10,
+  localparam WORDS_PER_BEAT = BUS_W/WORD_W
+)(
+  // system signals
+  input  logic clk,
+  // AXI-Stream
+  output logic                                  valid,
+  input  logic                                  ready,
+  output logic [WORDS_PER_BEAT-1:0][WORD_W-1:0] data
+);
+  // initialize stream
+  initial begin
+    valid = 1'b0;
+    data  = 'x;
+  end
 
-module AXIS_Sink #(
+  task axis_push_packet(
+    // reference data
+    input  logic [N_BEATS-1:0][WORDS_PER_BEAT-1:0][WORD_W-1:0] tx_data
+  );
+    // loop over beats
+    for (int i=0; i<N_BEATS; i++) begin
+      // randomize valid timing
+      while ($urandom_range(0,99) >= PROB_VALID) begin
+        @(posedge clk);
+      end
+      // drive stream
+      valid <= 1'b1;
+      data  <= tx_data[i];
+      // wait for transfer
+      do begin
+        @(posedge clk);
+      end while (~(valid & ready));
+      // clear the valid signal
+      valid <= 1'b0;
+      data  <= 'x;
+    end
+  endtask: axis_push_packet
+endmodule: AXIS_TX
+
+
+module AXIS_RX #(
   parameter  WORD_W=8, BUS_W=8, PROB_READY=20,
              N_BEATS=10,
              WORDS_PER_BEAT = BUS_W/WORD_W
 )(
-    input  logic clk, m_valid,
-    output logic m_ready=0,
-    input  logic [WORDS_PER_BEAT-1:0][WORD_W-1:0] m_data,
-    output logic [N_BEATS-1:0][WORDS_PER_BEAT-1:0][WORD_W-1:0] out_data
+  // system signals
+  input  logic clk,
+  // AXI-Stream
+  input  logic                                  valid,
+  output logic                                  ready,
+  input  logic [WORDS_PER_BEAT-1:0][WORD_W-1:0] data
 );
-  int i_beats = 0;
-  bit done = 0;
-  
-  task axis_pull_packet;
-    while (!done) begin
-      
-      @(posedge clk)
-      if (m_ready && m_valid) begin  
-// read at posedge
-        out_data[i_beats] = m_data;
-        i_beats += 1;
-        done = (i_beats == N_BEATS);
+  // initialize stream
+  initial begin
+    ready = 1'b0;
+  end
+
+  task axis_pull_packet(
+    // sampled data
+    output logic [N_BEATS-1:0][WORDS_PER_BEAT-1:0][WORD_W-1:0] data_o
+  );
+    // loop over beats
+    for (int i=0; i<N_BEATS; i++) begin
+      while ($urandom_range(0,99) >= PROB_READY) begin
+        @(posedge clk);
       end
-
-      #10ps m_ready = ($urandom_range(0,99) < PROB_READY);
+      ready <= 1'b1;
+      // wait for transfer
+      do begin
+        @(posedge clk);
+      end while (~(valid & ready));
+      // sample stream
+      data_o[i] <= data;
+      // clear the ready signal
+      ready <= 1'b0;
+      //$display("data[%0d]=%h=%h", i, data, data_o[i]);
     end
-    {m_ready, i_beats, done} ='0;
-  endtask
-endmodule
-
-
-
-module AXIS_Source #(
-  parameter  WORD_W=8, BUS_W=8, PROB_VALID=20, 
-             N_BEATS=10,
-  localparam WORDS_PER_BEAT = BUS_W/WORD_W
-)(
-    input  logic [N_BEATS-1:0][WORDS_PER_BEAT-1:0][WORD_W-1:0] in_data,
-    input  logic clk, s_ready, 
-    output logic s_valid=0,
-    output logic [WORDS_PER_BEAT-1:0][WORD_W-1:0] s_data='0
-);
-  int i_beats = 0;
-  bit prev_handshake = 1; 
-// data is released first
-  bit done = 0;
-  logic [WORDS_PER_BEAT-1:0][WORD_W-1:0] s_data_val;
-
-  task axis_push_packet;
-    
-// iverilog doesnt support break. so the loop is rolled to have break at top
-    while (!done) begin
-      if (prev_handshake) begin  
-// change data
-        s_data_val = in_data[i_beats];
-        i_beats    += 1;
-      end
-      s_valid = $urandom_range(0,99) < PROB_VALID;      
-// randomize s_valid
-      
-// scramble data signals on every cycle if !valid to catch slave reading it at wrong time
-      s_data = s_valid ? s_data_val : 'x;
-
-      
-// -------------- LOOP BEGINS HERE -----------
-      @(posedge clk);
-      prev_handshake = s_valid && s_ready; 
-// read at posedge
-      done           = s_valid && s_ready && (i_beats==N_BEATS);
-      
-      #10ps; 
-// Delay before writing s_valid, s_data, s_keep
-    end
-    {s_valid, s_data, i_beats, done} = '0;
-    prev_handshake = 1;
-  endtask
-endmodule
+    #1;
+    //$display("data_o %h", data_o);
+  endtask: axis_pull_packet
+endmodule: AXIS_RX
