b5f04709e3a605fc7651cbaab45f49085fe94059
Iztok Jeras <iztok.jeras@gmail.com>
2026-01-31 23:40:26 +0100

    issue with power operator used with vector signals

--- yosys-slang/issue/build.sh
+++ yosys-slang/issue/build.sh
@@ -3,12 +3,13 @@
 PRJ=binary2thermometer
 
 # test both implementations
-for imp in "POWER" "SHIFT"; do
-    yosys --plugin slang --logfile ${PRJ}_${imp}.syn.log -p \
+for imp in "POWER" "SHIFT"
+do
+    yosys --plugin slang --logfile ${PRJ}_${imp}.log -p \
     "read_slang ${PRJ}.sv --top ${PRJ} -G IMPLEMENTATION=\"${imp}\"; \
      proc; opt -full; \
-     write_json ${PRJ}_${imp}.json
+     write_json ${PRJ}_${imp}.json; \
      write_verilog ${PRJ}_${imp}.verilog"
-    exec netlistsvg ${PRJ}_${imp}.json -o ${PRJ}_${imp}.svg
+    netlistsvg ${PRJ}_${imp}.json -o ${PRJ}_${imp}.svg
 done
 
