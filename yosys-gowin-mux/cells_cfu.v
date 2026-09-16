8682070dae913064b18e99acd3aafbd28c0eee4a
Iztok Jeras <iztok.jeras@gmail.com>
2026-09-16 13:30:49 +0200

    Gowin: some cleanup

--- yosys-gowin-mux/cells_cfu.v
+++ yosys-gowin-mux/cells_cfu.v
@@ -17,6 +17,7 @@ module LUT3 #(parameter [7:0] INIT = 8'b0000_0000) (output F, input I0, I1, I2);
 	LUT4 #(.INIT(INIT4)) lut3 (.F(F), .I0(1'b0), .I1(I0), .I2(I1), .I3(I2));
 endmodule
 
+// LUT4 is a P&R primitive defined elsewhere
 
 module LUT5 #(parameter INIT = 32'h0000_0000) (output F, input I0, I1, I2, I3, I4);
 	wire f0, f1;
@@ -46,6 +47,7 @@ module LUT8 #(parameter INIT = 256'h0000_0000_0000_0000_0000_0000_0000_0000_0000
 	MUX2_LUT8 mux8(.I0(f0), .I1(f1), .S0(I7), .O(Y));
 endmodule
 
+// MUX2_LUT* are P&R primitives defined elsewhere
 
 module MUX2_MUX4 (input I0, I1, input S0, output O);
 	MUX2_LUT5 mux2_lut5 (.O(O), .I0(I0), .I1(I1), .S0(S0));
@@ -90,28 +92,32 @@ endmodule
 
 module MUX4 (output O, input I0, I1, I2, I3, input S0, S1);
     wire o0, o1;
-    MUX2 mux2_0 (.O(o0), .I0(I0), .I1(I1), .S0(S0));
-    MUX2 mux2_1 (.O(o1), .I0(I2), .I1(I3), .S0(S0));
+	//           O , I0, I1, S0
+    MUX2 mux2_0 (o0, I0, I1, S0);
+    MUX2 mux2_1 (o1, I2, I3, S0);
     MUX2_MUX4 mux2_o (.O(O), .I0(o0), .I1(o1), .S0(S0));
 endmodule
 
 module MUX8 (output O, input I0, I1, I2, I3, I4, I5, I6, I7, input S0, S1, S2);
     wire o0, o1;
-    MUX4 mux4_0 (.O(o0), .I0(I0), .I1(I1), .I2(I2), .I3(I3), .S0(S0), .S1(S1));
-    MUX4 mux4_1 (.O(o1), .I0(I4), .I1(I5), .I2(I6), .I3(I7), .S0(S0), .S1(S1));
+	//           O , I0, I1, I2, I3, S0, S1
+    MUX4 mux4_0 (o0, I0, I1, I2, I3, S0, S1);
+    MUX4 mux4_1 (o1, I4, I5, I6, I7, S0, S1);
     MUX2_MUX8 mux2_o (.O(O), .I0(o0), .I1(o1), .S0(S2));
 endmodule
 
 module MUX16 (output O, input I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15, input S0, S1, S2, S3);
     wire o0, o1;
-    MUX8 mux8_0 (.O(o0), .I0(I0), .I1(I1), .I2(I2 ), .I3(I3 ), .I4(I4 ), .I5(I5 ), .I6(I6 ), .I7(I7 ), .S0(S0), .S1(S1), .S2(S2));
-    MUX8 mux8_1 (.O(o1), .I0(I8), .I1(I9), .I2(I10), .I3(I11), .I4(I12), .I5(I13), .I6(I14), .I7(I15), .S0(S0), .S1(S1), .S2(S2));
+	//           O , I0, I1, I2 , I3 , I4 , I5 , I6 , I7 , S0, S1, S2
+    MUX8 mux8_0 (o0, I0, I1, I2 , I3 , I4 , I5 , I6 , I7 , S0, S1, S2);
+    MUX8 mux8_1 (o1, I8, I9, I10, I11, I12, I13, I14, I15, S0, S1, S2);
     MUX2_MUX16 mux2_o (.O(O), .I0(o0), .I1(o1), .S0(S3));
 endmodule
 
 module MUX32 (output O, input I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15, I16, I17, I18, I19, I20, I21, I22, I23, I24, I25, I26, I27, I28, I29, I30, I31, input S0, S1, S2, S3, S4);
     wire o0, o1;
-    MUX16 mux16_0 (.O(o0), .I0(I0 ), .I1(I1 ), .I2(I2 ), .I3(I3 ), .I4(I4 ), .I5(I5 ), .I6(I6 ), .I7(I7 ), .I8(I8 ), .I9(I9 ), .I10(I10), .I11(I11), .I12(I12), .I13(I13), .I14(I14), .I15(I15), .S0(S0), .S1(S1), .S2(S2), .S3(S3));
-    MUX16 mux16_1 (.O(o1), .I0(I16), .I1(I17), .I2(I18), .I3(I19), .I4(I20), .I5(I21), .I6(I22), .I7(I23), .I8(I24), .I9(I25), .I10(I26), .I11(I27), .I12(I28), .I13(I29), .I14(I30), .I15(I31), .S0(S0), .S1(S1), .S2(S2), .S3(S3));
+	//             O , I0 , I1 , I2 , I3 , I4 , I5 , I6 , I7 , I8 , I9 , I10, I11, I12, I13, I14, I15, S0, S1, S2, S3
+    MUX16 mux16_0 (o0, I0 , I1 , I2 , I3 , I4 , I5 , I6 , I7 , I8 , I9 , I10, I11, I12, I13, I14, I15, S0, S1, S2, S3);
+    MUX16 mux16_1 (o1, I16, I17, I18, I19, I20, I21, I22, I23, I24, I25, I26, I27, I28, I29, I30, I31, S0, S1, S2, S3);
     MUX2_MUX32 mux2_o (.O(O), .I0(o0), .I1(o1), .S0(S4));
 endmodule
